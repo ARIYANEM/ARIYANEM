d36bbf43357903f675cade834373044fcb914c60
ARIYANEM <emami8637@gmail.com>
2022-02-03 02:18:58 +0330

    class 13

--- articles/views.py
+++ articles/views.py
@@ -3,8 +3,10 @@ from . import models
 
 
 def articles_list(request):
-    articles = models.Article.objects.all().order_by('date')
+    articles = models.Article.objects.all().order_by('-date')
     collect = {'articles': articles}
     return render(request, 'articles/articleslist.html', collect)
 def detail(request, slug):
-    return HttpResponse(slug)
+    #return HttpResponse(slug)
+    Link = models.Article.objects.get(slug=slug)
+    return render(request, 'articles/articles_detail.html',{'Linker':Link})
